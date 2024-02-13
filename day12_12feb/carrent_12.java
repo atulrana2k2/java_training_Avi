9065c36227cfc88c5f3e07b74d9ba8992ec78356
atulrana2k2 <atul.rana@avisoft.io>
2024-02-13 11:30:46 +0530

    12feb

--- day12_12feb/carrent_12.java
+++ day12_12feb/carrent_12.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-abstract class Vechile{
+abstract class Vechile{                     //class vehicle having properties of vehicle
      String make;
     String model;
     int year;
     double rental_price;
 
-    public Vechile(String make, String model, int year, double rental_price) {
+    public Vechile(String make, String model, int year, double rental_price) {      //constructor fo Vehicle
         this.make = make;
         this.model = model;
         this.year = year;
@@ -18,23 +18,23 @@ abstract class Vechile{
 
 
 }
-class Car extends Vechile{
+class Car extends Vechile{                                      //Car class extends form Vehicle
     private int num_seats;
     private String fuel_type;
 
     public Car(String make, String model, int year, double rental_price, int num_seats, String fuel_type) {
         super(make, model, year, rental_price);
-        this.num_seats = num_seats;
+        this.num_seats = num_seats;                                 //construtor to chile and base class variables
         this.fuel_type = fuel_type;
     }
 
 
 }
-class Customer{
+class Customer{                                                 //class customer having properties of customer
     private String name;
     private String email;
 
-    public Customer(String name, String email) {
+    public Customer(String name, String email) {                //customer constructor
         this.name = name;
         this.email = email;
 
@@ -42,7 +42,7 @@ class Customer{
 
     public String getName(){
         return name;
-    }
+    }               //accesing private members of the class using member function
     public String getEmail(){
         return email;
     }
@@ -52,9 +52,9 @@ class Customer{
 public class carrent_12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Vechile> vechiles = new ArrayList<>();
-        List<Customer> coustomers = new ArrayList<>();
-        List<Vechile> rented = new ArrayList<>();
+        List<Vechile> vechiles = new ArrayList<>();                 //List containing Vehicle objects
+        List<Customer> coustomers = new ArrayList<>();              //List containing Customer objects
+        List<Vechile> rented = new ArrayList<>();                   //List containing object of Vehicle which are Rented
         while (true) {
             System.out.println("OPTIONS");
             System.out.println("1.Enter Vechiles");
@@ -68,13 +68,13 @@ public class carrent_12 {
             int choice = sc.nextInt();
             switch (choice) {
                 case 1:
-                    Car vechile = addvechile(sc);
+                    Car vechile = addvechile(sc);                            //making new object of Vehicle
                     if (vechile != null)
-                        vechiles.add(vechile);
+                        vechiles.add(vechile);                                //adding object to vehicles list
                     System.out.println("Vehicle added successfully");
                     break;
                 case 2: {
-                    if (!vechiles.isEmpty()) {
+                    if (!vechiles.isEmpty()) {                                  //displaying vehicles present by vehicles list
                         System.out.println("Vehicles available for rent:");
                         int i=1;
                         for (Vechile present : vechiles) {
@@ -85,13 +85,13 @@ public class carrent_12 {
                     break;
                 }
                 case 3:
-                    Customer customer = addcustomer(sc);
+                    Customer customer = addcustomer(sc);                //creating object of customer class
                     if (customer != null)
-                        coustomers.add(customer);
+                        coustomers.add(customer);                     //adding Customer object to customers list
                     System.out.println("Customer added successfully");
                     break;
                 case 4: {
-                    if (!coustomers.isEmpty()) {
+                    if (!coustomers.isEmpty()) {                        //printing the Customer in customers list
                         System.out.println("Registered customers are :");
                         int i=1;
                         for (Customer present : coustomers) {
@@ -106,20 +106,20 @@ public class carrent_12 {
                     System.out.println("Enter your registerd email:");
                     String email = sc.next();
                     for (Customer rcustomer : coustomers) {
-                        if (rcustomer.getEmail().equalsIgnoreCase(email)) {
+                        if (rcustomer.getEmail().equalsIgnoreCase(email)) {                         //checking customer exist in the system or not
                             userr = true;
                             System.out.println("Enter the vehicle model(name of Vehicle)");
                             String vechilemodel = sc.next();
                             boolean flag = false;
                             for (Vechile entry : vechiles) {
-                                if (entry.model.equalsIgnoreCase(vechilemodel)) {
+                                if (entry.model.equalsIgnoreCase(vechilemodel)) {               //checking model required by the customer is present or not
                                     System.out.println("Vechile Rented to " + rcustomer.getName() + " " + entry.make + " : " + entry.model);
                                     System.out.print("Enter the number of day you want to rent the Vechile:");
                                     int days = sc.nextInt();
                                     System.out.println("Total rent=" + entry.rental_price * days);
                                     flag = true;
-                                    rented.add(entry);
-                                    vechiles.remove(entry);
+                                    rented.add(entry);                                      //add the Vehicle to the rented list
+                                    vechiles.remove(entry);                                 //remove it from the vehicle list available
                                     break;
 
                                 } else
@@ -147,8 +147,8 @@ public class carrent_12 {
                             for (Vechile check : rented) {
                                 if (check.model.equalsIgnoreCase(vehiclemodel2)) {
                                     System.out.println("vehcile " + check.make + " " + check.model + " is  returned");
-                                    rented.remove(check);
-                                    vechiles.add(check);
+                                    rented.remove(check);                       //remove vehicle from rented list
+                                    vechiles.add(check);                        //add vehicle to vehicles  present for rent list
                                     returnv = true;
                                     break;
 
@@ -189,7 +189,7 @@ public class carrent_12 {
         System.out.println("Enter type of fuel: Diesel(D) Petrol(P) (CNG) :");
         String fuel= sc.next();
         sc.nextLine();
-        return new Car(model,make,year,price,seats,fuel);
+        return new Car(model,make,year,price,seats,fuel);                   //returning a new object of Car
     }
     public static Customer addcustomer(Scanner sc)
     {
@@ -199,6 +199,6 @@ public class carrent_12 {
         System.out.println("Enter the email id of the customer:");
         String emailid= sc.next();
         sc.nextLine();
-        return new Customer(name,emailid);
+        return new Customer(name,emailid);                                  //returning a new object of Customer
     }
 }
